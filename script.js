82615f8ab5f084dc45dd915bcb7be2d5ca77a040
Lucy Toman <lucy@toman.me.uk>
2020-06-22 16:15:14 +0200

    adds contact links

--- script.js
+++ script.js
@@ -5,17 +5,22 @@ $(".self").html(
 );
 $(".name").html("Lucy Toman");
 $(".lang").html('<button id="en">🇬🇧</button> <button id="de"> 🇩🇪</button> ');
-$(".theme").html(
-    '<button id="theme" onclick="toggleTheme()">dark mode</button>'
+$(".contact-me").html(
+    `<a href="mailto:lucy.toman+website@gmail.com"><i class="fa fa-envelope"> </i> email </a> <a href="https://github.com/lucywho"><i class='fab fa-github'> </i> GitHub </a> <a href="https://www.linkedin.com/in/lucytoman/"><i class='fab fa-linkedin'> </i> LinkedIn</a>`
 );
+
+$(".theme").html('<button id="theme" onclick="toggleTheme()">🌒</button>');
 
 function start() {
     $(".projects-header").html("Recent Projects");
     $(".text").html("Welcome to my website!");
-    $(".about-me").html(
+    $(".about-me-text").html(
         `<strong>HIRE ME!</strong></br></br>I'm a recent graduate of Spiced Academy's full stack web development course. I write in vanilla JavaScript and jQuery, with Node.js. I have built projects using Handlebars, Vue and React, and have created databases with postgreSQL.Now I'm looking for new challenges as a junior developer.</br></br>
         I have a degree in Education and 15 years experience as a policy advisor and project manager in the UK Civil Service. Native English-speaker with B1+ German</br></br>
-        <em>Note: This website is still under construction and some "Recent Projects" link to github respositories.</em>`
+        `
+    );
+    $(".under-construction").html(
+        `<em>Note: This website is still under construction and some "Recent Projects" link to github respositories.</em>`
     );
 }
 
@@ -31,11 +36,14 @@ $("#de").click(function() {
     $(".projects-header").html("Aktuelle Projekte");
     $(".text").html("Wilkommen auf meiner Website!");
 
-    $(".about-me").html(
+    $(".about-me-text").html(
         `<strong>Ich bin die Richtige für Ihr Team!</strong></br></br>Ich habe den Full-Stack-Web-Developer-Kurs der Spiced Academy absolviert. Ich schreibe in Vanilla JavaScript und jQuery mit Node.js. Ich habe Projekte mit Handlebars, Vue und React sowie Datenbanken mit PostgreSQL erstellt. Ich suche jetzt nach neuen Herausforderungen als Junior Developer.
         </br></br>
         Ich habe einen Abschluss in Bildung und kann 15 Jahre Erfahrung als Politikberaterin und Projektmanagerin im britischen öffentlichen Dienst vorweisen.</br></br>
-       <em> Hinweis: Diese Website befindet sich noch im Aufbau und einige "Aktuelle Projekte" verlinken auf Github Repositories.</em>`
+      `
+    );
+    $(".under-construction").html(
+        `<em> Hinweis: Diese Website befindet sich noch im Aufbau und einige "Aktuelle Projekte" verlinken auf Github Repositories.</em>`
     );
 });
 
@@ -44,13 +52,13 @@ function toggleTheme() {
         theme = "dark.css";
         $("head link#style").attr("href", theme);
         $(".theme").html(
-            '<button id="theme" onclick="toggleTheme()">light mode</button>'
+            '<button id="theme" onclick="toggleTheme()">☀️</button>'
         );
     } else {
         theme = "style.css";
         $("head link#style").attr("href", theme);
         $(".theme").html(
-            '<button id="theme" onclick="toggleTheme()">dark mode</button>'
+            '<button id="theme" onclick="toggleTheme()">🌒</button>'
         );
     }
 }
