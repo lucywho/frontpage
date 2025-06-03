c43000ea648a2bb780aac55a86c439dbec9d280e
Lucy Toman <lucy.toman@thinslices.com>
2025-06-03 17:56:08 +0200

    fixes language change on nav buttons, starts refactoring into modules

---
+++ components/welcome.js
@@ -0,0 +1,7 @@
+export const welcomeText = function () {
+  document.getElementById(
+    'contents'
+  ).innerHTML = `<div class="welcome cont-default"><h2>Welcome to my website</h2></br></br><p>A gallery of stuff that I'm doing or thinking about</p>
+        </br><p>Take a look around and get in touch!</p>
+     </div>`;
+};
